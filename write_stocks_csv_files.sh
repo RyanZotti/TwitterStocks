e614517920545753d69303f8f36aff4118845b7a
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2016-02-06 16:49:17 -0500

    Tables to plain csv

---
+++ write_stocks_csv_files.sh
@@ -0,0 +1,9 @@
+MYSQL_PATH="/usr/local/mysql/bin"
+MYSQL_USER="root"
+MYSQL_DATABASE="screenscraping"
+MYSQL_HOST="localhost"
+TARGET_DIR="/TwitterData/2013_bkp/stocks"
+TABLES=`/usr/local/mysql/bin/mysql -u root screenscraping -h localhost -s -e "select * from (SELECT table_name FROM information_schema.tables WHERE table_schema = DATABASE()) as tbl where table_name like 'stocks%'"`
+for TABLE in $TABLES; do
+  ${MYSQL_PATH}/mysql -u ${MYSQL_USER} ${MYSQL_DATABASE} -h ${MYSQL_HOST} -e "select * from $TABLE INTO OUTFILE '${TARGET_DIR}/${TABLE}.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '\"'LINES TERMINATED BY '\n';"
+done
